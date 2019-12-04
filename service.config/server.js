de4581d1c74186c1ce9854389f71eab698b6f35a
Olili2017 <olilidaniel48@gmail.com>
2019-12-04 09:15:38 +0300

    (Config) : change config to hieracy mode

--- service.config/server.js
+++ service.config/server.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use((req,res,next) => {
     res.header("Access-Control-Allow-Origin", "172.22.0.2")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept")
-    console.log(`Incoming ${req.method} -- from ${req.ip} with body ${JSON.stringify(req.body)}`)
+    console.log(`${req.method} ${req.originalUrl} ${JSON.stringify(req.body)}`)
     next()
 })
 
